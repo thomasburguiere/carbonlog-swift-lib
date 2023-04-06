288702a79b50bbfe24dfa71da0ce23bc1cf32488
Thomas Burguiere <thomas.burguiere@gmail.com>
2023-04-06 13:08:24 +0200

    fix: test fix

--- Tests/CarbonLogLibTests/CarbonMeasurementTests.swift
+++ Tests/CarbonLogLibTests/CarbonMeasurementTests.swift
@@ -5,7 +5,7 @@ import XCTest
 final class CarbonMeasurementTests: XCTestCase {
     let date1 =
         calendar
-        .date(from: DateComponents(year: 2022, month: 1, day: 1))!
+        .date(from: DateComponents(year: 2022, month: 4, day: 6, hour: 12))!
     func test_init() {
         var ms = CarbonMeasurement(kg: 0.0, at: date1)
 
@@ -14,6 +14,6 @@ final class CarbonMeasurementTests: XCTestCase {
         ms = CarbonMeasurement(by: CarbonEquivalent(planeKm: 2000.0), at: date1)
         XCTAssertEqual(ms.carbonKg.round(to: 2), 371.75)
 
-        XCTAssertEqual(ms.description, "371.747212 at 2021-12-31 23:00:00 +0000")
+        XCTAssertTrue(ms.description.contains("371.747212 at 2022-04-06"))
     }
 }
