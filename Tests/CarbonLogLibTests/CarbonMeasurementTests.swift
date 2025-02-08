7799c552bdcb372c56618ea65465c223f6901a16
Thomas Burguiere <thomas.burguiere@gmail.com>
2025-02-08 12:52:21 +0100

    Feature/migrate test (#5)
    * chore(config): migrate config renovate.json (#4)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * fix: migrating XCTtest -> Swift Testing
    
    * ci: build swift 6
    
    * ci: use different gh action
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Thomas Burguiere <thomas.burguiere1@swisscom.com>

--- Tests/CarbonLogLibTests/CarbonMeasurementTests.swift
+++ Tests/CarbonLogLibTests/CarbonMeasurementTests.swift
@@ -1,36 +1,37 @@
-import XCTest
+import Foundation
+import Testing
 
 @testable import CarbonLogLib
 
-final class CarbonMeasurementTests: XCTestCase {
+struct CarbonMeasurementTests {
     let date1 =
         calendar
         .date(from: DateComponents(year: 2022, month: 4, day: 6, hour: 12))!
-    func test_init() {
+    @Test func test_init() {
         var ms = CarbonMeasurement(kg: 0.0, at: date1)
 
-        XCTAssertEqual(ms.carbonKg, 0.0)
+        #expect(ms.carbonKg == 0.0)
 
         ms = CarbonMeasurement(by: CarbonEquivalent(planeKm: 2000.0), at: date1)
-        XCTAssertEqual(ms.carbonKg.round(to: 2), 371.75)
+        #expect(ms.carbonKg.round(to: 2) == 371.75)
 
-        XCTAssertTrue(
-            ms.description.starts(with: "371.747212 at 2022-04-06"),
-            "expected description to start with \"371.747212 at 2022-04-06\", actual \"\(ms.description)\""
+        #expect(
+            ms.description.starts(with: "371.747212 at 2022-04-06")//,
+            // "expected description to start with \"371.747212 at 2022-04-06\", actual \"\(ms.description)\""
         )
 
         let now = Date()
         ms = CarbonMeasurement(carbonKg: 2.5)
-        XCTAssertTrue(
-            ms.description.starts(with: "2.5"),
-            "expected description to start with \"2.5\", actual: \"\(ms.description)\""
+        #expect(
+            ms.description.starts(with: "2.5")//,
+            // "expected description to start with \"2.5\", actual: \"\(ms.description)\""
         )
-        XCTAssertTrue(ms.date >= now)
+        #expect(ms.date >= now)
 
         ms = CarbonMeasurement(by: CarbonEquivalent(planeKm: 2000.0))
-        XCTAssertEqual(ms.carbonKg.round(to: 2), 371.75)
+        #expect(ms.carbonKg.round(to: 2) == 371.75)
 
         ms = CarbonMeasurement(carbonKg: 371.75)
-        XCTAssertEqual(ms.equivalent.planeKm.round(to: 0), 2000.00)
+        #expect(ms.equivalent.planeKm.round(to: 0) == 2000.00)
     }
 }
