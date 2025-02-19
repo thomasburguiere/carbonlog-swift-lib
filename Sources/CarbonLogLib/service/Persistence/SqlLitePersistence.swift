0f2f6de6dc9ed7d28653def854f151f6af47e519
Thomas Burguiere <thomas.burguiere@gmail.com>
2025-02-19 07:36:50 +0100

    wip select, missing comment insert

--- Sources/CarbonLogLib/service/Persistence/SqlLitePersistence.swift
+++ Sources/CarbonLogLib/service/Persistence/SqlLitePersistence.swift
@@ -1,6 +1,11 @@
 import Foundation
 import SQLite3
 
+public enum SQLError: Error {
+    case CouldNotPrepareStatement
+    case InconsistentRow
+}
+
 public struct SQLitePersistenceService: CarbonLogPersistenceService {
     let formatter = ISO8601DateFormatter()
     let dbFilePath: URL
@@ -11,35 +16,58 @@ public struct SQLitePersistenceService: CarbonLogPersistenceService {
 
     public func persist(log _: CarbonLog) async throws {}
 
-    public func persist(measurement: CarbonMeasurement) async throws {
+    public func persist(measurement: CarbonMeasurement, id: String? = nil) async throws {
         let db = openDatabase(filepath: dbFilePath.absoluteString)
         var insertStatement: OpaquePointer?
         let insertStatementString = """
           INSERT INTO CarbonMeasurement (id, carbonKg, date) VALUES (?, ?, ?);
         """
-        if sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) ==
-            SQLITE_OK
-        {
-            let id: NSString = UUID().uuidString as NSString
-            let carbonKg = measurement.carbonKg
-            let date: NSString = formatter.string(from: measurement.date) as NSString
-            // 2
-            sqlite3_bind_text(insertStatement, 1, id.utf8String, -1, nil)
-            sqlite3_bind_double(insertStatement, 2, carbonKg)
-            sqlite3_bind_text(insertStatement, 3, date.utf8String, -1, nil)
-            // 4
-            if sqlite3_step(insertStatement) == SQLITE_DONE {
-                print("\nSuccessfully inserted row.")
-            } else {
-                print("\nCould not insert row.")
-            }
+        guard sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK else {
+            throw SQLError.CouldNotPrepareStatement
+        }
+        let id: NSString = (id ?? UUID().uuidString) as NSString
+        let carbonKg = measurement.carbonKg
+        let date: NSString = formatter.string(from: measurement.date) as NSString
+        // 2
+        sqlite3_bind_text(insertStatement, 1, id.utf8String, -1, nil)
+        sqlite3_bind_double(insertStatement, 2, carbonKg)
+        sqlite3_bind_text(insertStatement, 3, date.utf8String, -1, nil)
+        // 4
+        if sqlite3_step(insertStatement) == SQLITE_DONE {
+            print("\nSuccessfully inserted row.")
         } else {
-            print("\nINSERT statement is not prepared.")
+            print("\nCould not insert row.")
         }
     }
 
     public func load(id _: String) async -> CarbonLog? {
         return nil
+    }
+
+    public func load(measurementId id: String) throws -> CarbonMeasurement? {
+        let db = openDatabase(filepath: dbFilePath.absoluteString)
+        var selectStatement: OpaquePointer?
+        let selectStatementString = """
+          select date, carbonKg, comment from CarbonMeasurement where id = ?;
+        """
+        guard sqlite3_prepare_v2(db, selectStatementString, -1, &selectStatement, nil) == SQLITE_OK else {
+            throw SQLError.CouldNotPrepareStatement
+        }
+
+        let nsId: NSString = id as NSString
+
+        sqlite3_bind_text(selectStatement, 1, nsId.utf8String, -1, nil)
+        guard sqlite3_step(selectStatement) == SQLITE_ROW else { return nil }
+
+        let date: Date? = sqlite3_column_text(selectStatement, 0)
+            .flatMap { formatter.date(from: String(cString: $0)) }
+        let carbonKg: Double? = sqlite3_column_double(selectStatement, 1)
+        let comment: String? = sqlite3_column_text(selectStatement, 2)
+            .flatMap { String(cString: $0) }
+
+        guard let date, let carbonKg else { throw SQLError.InconsistentRow }
+
+        return CarbonMeasurement(kg: carbonKg, at: date, comment: comment)
     }
 
     public func append(measurement _: CarbonMeasurement, toLogWithId _: String) async throws {}
--- Tests/CarbonLogLibTests/service/Persistence/SQLiteFilePersistenceTests.swift
+++ Tests/CarbonLogLibTests/service/Persistence/SQLiteFilePersistenceTests.swift
@@ -13,7 +13,7 @@ private let date2 =
 
 @Suite("SQLite Persistence")
 struct name {
-    private let cm2 = CarbonMeasurement(kg: 42.0, at: date2)
+    private let cm2 = CarbonMeasurement(kg: 42.0, at: date2, comment: "kurwa comment")
     @Test func testName() async throws {
         let tempFolderURL = FileManager.default.temporaryDirectory
         let tempOutFileURL = tempFolderURL.appending(component: "test.sqlite")
@@ -38,6 +38,11 @@ struct name {
         let service = SQLitePersistenceService(dbPath: tempOutFileURL)
 
         // when
-        try await service.persist(measurement: cm2)
+        try await service.persist(measurement: cm2, id: "kurwa")
+        let persisted = try #require(try service.load(measurementId: "kurwa"))
+
+        #expect(persisted.carbonKg == 42)
+        #expect(persisted.date.description == "2022-01-02 12:00:00 +0000")
+        #expect(persisted.comment == "kurwa comment")
     }
 }
