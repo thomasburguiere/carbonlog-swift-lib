46879d5df1c3df0dbb005488d965a36967e3aa0c
Thomas Burguiere <thomas.burguiere@gmail.com>
2025-02-26 19:32:48 +0100

    chore: adding framework dependency

--- Package.swift
+++ Package.swift
@@ -5,8 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "CarbonLogLib",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v12),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible
+        // to other packages.
         .library(
             name: "CarbonLogLib",
             targets: ["CarbonLogLib"]
@@ -16,13 +21,16 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3"),
+        .package(url: "https://github.com/hmlongco/Factory", from: "2.4.3"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets are the basic building blocks of a package. A target can define a module or a
+        // test suite.
+        // Targets can depend on other targets in this package, and on products in packages this
+        // package depends on.
         .target(
             name: "CarbonLogLib",
-            dependencies: []
+            dependencies: ["Factory"]
         ),
         .testTarget(
             name: "CarbonLogLibTests",
